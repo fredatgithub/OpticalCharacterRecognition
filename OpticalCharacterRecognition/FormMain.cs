6e2c6fb733005ff81902a47b9a232bcce1399f5d
fred <domaine75@gmail.com>
2015-12-30 00:03:37 +0100

    Update FormMain.cs

--- OpticalCharacterRecognition/FormMain.cs
+++ OpticalCharacterRecognition/FormMain.cs
@@ -535,7 +535,7 @@ namespace OpticalCharacterRecognition
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
